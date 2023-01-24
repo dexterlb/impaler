65710a27b3e9b8f8acbf78fc06db6df62771f974
dexterlb <dexterlb@qtrp.org>
2023-01-24 20:32:58 +0200

    make build.sh not rely on $0

--- tex/build.sh
+++ tex/build.sh
@@ -6,28 +6,32 @@ cd "$(dirname "$(readlink -f "${0}")")"
 
 OPTS=(-latexoption="-shell-escape" -halt-on-error -xelatex)
 
-case "${1}" in
-    "")
-        "${0}" build main.tex
-        ;;
-    build)
-        latexmk ${OPTS[@]} "${2}"
-        ;;
-    clean)
-        latexmk -C
-        for f in *.bbl *.run.xml *.aux{,lock} *.log *.fls *-figure*.{pdf,log,dpth,md5}; do
-            rm -rvf "${f}"
-        done
-        ;;
-    watch)
-        if [[ -z "${2}" ]]; then
-            f=main.tex
-        else
-            f="${2}"
-        fi
-        latexmk ${OPTS[@]} -pvc "${f}"
-        ;;
-    *)
-        echo "say whaaa?"
-        ;;
-esac
+function choice {
+    case "${1}" in
+        "")
+            choice build main.tex
+            ;;
+        build)
+            latexmk ${OPTS[@]} "${2}"
+            ;;
+        clean)
+            latexmk -C
+            for f in *.bbl *.run.xml *.aux{,lock} *.log *.fls *-figure*.{pdf,log,dpth,md5}; do
+                rm -rvf "${f}"
+            done
+            ;;
+        watch)
+            if [[ -z "${2}" ]]; then
+                f=main.tex
+            else
+                f="${2}"
+            fi
+            latexmk ${OPTS[@]} -pvc "${f}"
+            ;;
+        *)
+            echo "say whaaa?"
+            ;;
+    esac
+}
+
+choice "${@}"
