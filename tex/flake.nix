ab868d846e6c57975447fb9abec57092f835d851
dexterlb <dexterlb@qtrp.org>
2023-01-24 20:33:16 +0200

    add nix files

--- tex/flake.nix
+++ tex/flake.nix
@@ -1,26 +1,71 @@
 {
   description = "Thesis text";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      packages.x86_64-linux.default = pkgs.runCommand
-        "thesis-text"
-        { }
-        ''
-          # ${pkgs.nodePackages.reveal-md}/bin/reveal-md --static $out ${./present.md}
-          echo foo
-        '';
-      devShell.x86_64-linux = pkgs.mkShell
-        {
-          packages = [
-            pkgs.emacsPackages.auctex-latexmk
-          ];
-          shellHook = ''
-            echo 'foo bar baz'.
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex-megapkg = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small
+            latex-bin latexmk
+            extsizes euenc tools
+            hyperref pdftexcmds infwarerr
+            kvoptions l3kernel zref
+            fontspec
+            libertine
+            geometry titling mathabx csquotes
+            subfiles cleveref ebproof
+            appendix
+            minibox pbox mdframed needspace adjustbox
+            lstaddons
+            biblatex
+            babel-bulgarian babel-english;
+        };
+        deps = [
+          tex-megapkg
+          pkgs.biber
+        ];
+      in
+      {
+        packages.default = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "thesis-text";
+          src = "${self}/tex";
+          buildInputs = [
+            pkgs.coreutils
+            pkgs.bash
+          ] ++ deps;
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+
+            mkdir -p .cache/texmf-var
+            export TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var
+
+            bash build.sh
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp main.pdf $out/
           '';
         };
-    };
+        devShell = pkgs.mkShell
+          {
+            packages = [
+              tex-megapkg
+              pkgs.biber
+            ] ++ deps;
+            shellHook = ''
+              echo "you can use './build.sh' to build main.pdf"
+              echo "you can also use './build.sh watch' to build automatically on file change."
+            '';
+          };
+      }
+    );
 }
