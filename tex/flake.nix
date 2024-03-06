e3590d644b7a6592c54090777519a05964c86fc5
dexterlb <dexterlb@qtrp.org>
2024-03-06 16:34:25 +0200

    setup build for spring session slides

--- tex/flake.nix
+++ tex/flake.nix
@@ -4,68 +4,20 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
+    latex_tools.url = github:dexterlb/latex_tools;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib;
-    eachSystem allSystems (system:
+  outputs = { self, nixpkgs, flake-utils, latex_tools }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        tex-megapkg = pkgs.texlive.combine {
-          inherit (pkgs.texlive)
-            scheme-small
-            latex-bin latexmk
-            extsizes euenc tools
-            hyperref pdftexcmds infwarerr
-            kvoptions l3kernel zref
-            fontspec
-            libertine
-            geometry titling mathabx csquotes
-            subfiles cleveref ebproof
-            appendix
-            minibox pbox mdframed needspace adjustbox
-            lstaddons
-            biblatex
-            babel-bulgarian babel-english;
-        };
-        deps = [
-          tex-megapkg
-          pkgs.biber
-        ];
+        latexTools = latex_tools.lib.mkLatexTools { inherit nixpkgs pkgs; };
+        fmiSpringSession = (import ./fmi_spring_session) { inherit pkgs latexTools; };
       in
       {
-        packages.default = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "thesis-text";
-          src = ./.;
-          buildInputs = [
-            pkgs.coreutils
-            pkgs.bash
-          ] ++ deps;
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+        packages = fmiSpringSession.packages // {
 
-            mkdir -p .cache/texmf-var
-            export TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var
-
-            bash build.sh
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp main.pdf $out/
-          '';
         };
-        devShell = pkgs.mkShell
-          {
-            packages = [
-              tex-megapkg
-              pkgs.biber
-            ] ++ deps;
-            shellHook = ''
-              echo "you can use './build.sh' to build main.pdf"
-              echo "you can also use './build.sh watch' to build automatically on file change."
-            '';
-          };
       }
     );
 }
