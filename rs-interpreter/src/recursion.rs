7a5a0ba770570d46ba8ea84bfdcec4aa8ac6a21f
dexterlb <dexterlb@qtrp.org>
2026-08-13 14:29:26 +0300

    recursion wip

--- rs-interpreter/src/recursion.rs
+++ rs-interpreter/src/recursion.rs
@@ -1,50 +1,75 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 use crate::evaluator::apply;
 use crate::value_list::ValueList;
 use crate::values::{Cont, External, Value};
 
-pub fn poly_fix_list(funcs: &ValueList) -> ValueList {
-    Rec::build(funcs)
+type ListOfCallables = ValueList; // duckily assume that the user has provided values that are callable
+
+// Constructs a list of first-order functions ("funcs") from a list of
+// second-order functions ("operators") that each expect to be passed the funcs
+// as arguments and produce their respective result function
+pub fn poly_fix_list(operators: &ListOfCallables) -> ListOfCallables {
+    let rec_refs = Rc::new_cyclic(|weak: &Weak<ListOfCallables>| {
+        operators.map(|operator| RecRef::weak(weak, operator.clone()).val())
+    });
+    operators.map(|operator| RecRef::strong(&rec_refs, operator.clone()).val())
 }
 
-#[derive(Debug)]
-struct Rec {
-    funcs: ValueList,
+#[derive(Debug, Clone)]
+enum Rec {
+    Strong(Rc<ListOfCallables>),
+    Weak(Weak<ListOfCallables>),
 }
 
 impl Rec {
-    fn build(funcs: &ValueList) -> ValueList {
-        Rc::new(Rec {
-            funcs: funcs.clone(),
-        })
-        .rec_refs()
-    }
-
-    fn rec_refs(self: &Rc<Self>) -> ValueList {
-        let rec = self.clone();
-        self.funcs.map(|func| {
-            Value::external(RecRef {
-                rec: rec.clone(),
-                func: func.clone(),
-            })
-        })
+    fn funcs(&self) -> Rc<ListOfCallables> {
+        match self {
+            Rec::Strong(rec_refs) => rec_refs.clone(),
+            Rec::Weak(weak) => match weak.upgrade() {
+                Some(rec_refs) => rec_refs,
+                None => panic!("Rec used after being dropped, this must be impossible!"),
+            },
+        }
     }
 }
 
 #[derive(Debug)]
 struct RecRef {
-    rec: Rc<Rec>,
-    func: Value,
+    rec: Rec,
+    operator: Value,
+}
+
+impl RecRef {
+    fn weak(funcs: &Weak<ListOfCallables>, operator: Value) -> RecRef {
+        RecRef {
+            rec: Rec::Weak(funcs.clone()),
+            operator,
+        }
+    }
+
+    fn strong(funcs: &Rc<ListOfCallables>, operator: Value) -> RecRef {
+        RecRef {
+            rec: Rec::Strong(funcs.clone()),
+            operator,
+        }
+    }
+
+    fn val(self) -> Value {
+        Value::external(self)
+    }
 }
 
 impl External for RecRef {
-    fn apply(&self, gret: Cont, garg: ValueList) {
-        let rec_refs = self.rec.rec_refs();
+    fn apply(&self, ret: Cont, arg: ValueList) {
+        let funcs = self.rec.funcs();
+        let operator = self.operator.clone();
         apply(
-            Rc::new(move |g: Value| apply(gret.clone(), g, garg.clone())),
-            self.func.clone(),
-            rec_refs,
+            // first apply the operator to the funcs to obtain a func `f`,
+            // then apply f to the given arg
+            Rc::new(move |f: Value| apply(ret.clone(), f, arg.clone())),
+            operator,
+            (*funcs).clone(),
         );
     }
 
